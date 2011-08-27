5035f7a2a09052482692191df70830c852f0624e
mnylen <mikko.nylen@gmail.com>
2011-08-27 11:52:17 +0300

    Fixed initialization of game.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,45 +1,33 @@
-var gamejs = require('gamejs');
-
-var GameCanvas = function(el, width, height) {
-  this.el = el;
-  this.el.width = width;
-  this.el.height = height;
-  this.ctx = el.getContext('2d');
-};
-
-GameCanvas.prototype.fitToWindow = function() {
-  var areaWidth = $(window).width() - $('#sidebar').width() - 20
-    , areaHeight = $(window).height()
-    , resizeFactor = Math.min(areaWidth / this.el.width, areaHeight / this.el.height);
-
-  $(this.el).css({
-    width: this.el.width * resizeFactor,
-    height: this.el.height * resizeFactor
-  });
-};
-
-GameCanvas.prototype.clear = function() {
-  this.ctx.save();
-  this.ctx.setTransform(1, 0, 0, 1, 0, 0);
-  this.ctx.clearRect(0, 0, this.el.width, this.el.height);
-  this.ctx.restore();
-};
-
-
-GameCanvas.prototype.draw = function() {
-  this.clear();
-  // Just draw something to test the animation
-  var angle = new Date().getTime() % (Math.PI*2);
-  this.ctx.save();
-  this.ctx.fillRect(this.el.width/2 - 50 - Math.random() * 10, this.el.height/2 - 50 - Math.random() * 10, 100 + Math.random() * 10, 100 + Math.random() * 10);
-  this.ctx.restore();
-};
+var gamejs = require('gamejs'),
+	tick = require('tick');
 
 
 var main = function() {
+	$("#login").hide();
+	$("#main").show();
+	
+	var resizeToFitWindow = function() {
+		var canvas = $("#gjs-canvas")[0];
+		
+		var areaWidth = $(window).width() - $('#sidebar').width() - 20
+	    , areaHeight = $(window).height()
+	    , resizeFactor = Math.min(areaWidth / canvas.width, areaHeight / canvas.height);
+	  
+	  $(canvas).css({
+	    width: canvas.width * resizeFactor,
+	    height: canvas.height * resizeFactor
+	  });
+	};
+	
+				
+	display = gamejs.display.setMode([400, 400]);
+	resizeToFitWindow();
+		
+	tick.start(display);
+};
+
+$(document).ready(function() {
 	var socket = io.connect();
-	var display = null;
-	var gameCanvas = null;
 	
 	socket.on('gameState', function(state) {
     $('#player-list').empty();
@@ -48,27 +36,10 @@ var main = function() {
     });
   });
 	
-	var tick = function() {
-		gameCanvas.draw();
-	};
-	
-	var startGame = function() {
-		$("#login").hide();
-		$("#main").show();
-		
-		gameCanvas = new GameCanvas($('#gjs-canvas')[0], 400, 400);
-		display = gamejs.display.setMode([400, 400]);
-		gameCanvas.fitToWindow();
-		
-		gamejs.time.fpsCallback(tick, this, 26);		
-	};
-	
 	$('#start').click(function() {
     socket.emit('join', $('#nick').val(), function() {
-      startGame();
+      gamejs.preload([]);
+			gamejs.ready(main);
     });
   });
-};
-
-gamejs.preload([]);
-gamejs.ready(main);
+});
