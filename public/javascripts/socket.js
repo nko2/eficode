7da20d09b3af7021593fc6c774f62257d5a6ec0f
mnylen <mikko.nylen@gmail.com>
2011-08-27 14:01:56 +0300

    Moving pandas

--- public/javascripts/socket.js
+++
@@ -1,9 +0,0 @@
-var Socket = function() {
-	this.socket = io.connect();
-};
-
-Socket.prototype.join = function(nick, callback) {
-	this.socket.emit('join', nick, callback);
-};
-
-module.exports.Socket = Socket;
---
+++ public/javascripts/sprites.js
@@ -0,0 +1,34 @@
+var gamejs = require('gamejs');
+
+var Animal = function(rect) {
+	Animal.superConstructor.apply(this, arguments);
+	this.speed = 0;
+	this.rect = new gamejs.Rect(rect);
+	this.image = null;
+	this.images = [];
+	
+	return this;
+};
+gamejs.utils.objects.extend(Animal, gamejs.sprite.Sprite);
+
+Animal.prototype.setDirection = function(val) {
+	this.image = this.images[val];
+};
+
+Animal.prototype.move = function(x, y, dir) {
+	this.setDirection(dir);
+	this.rect.left = x;
+	this.rect.top = y;
+};
+
+var Panda = function(rect) {
+	Panda.superConstructor.apply(this, arguments);
+	this.images[0] = gamejs.image.load("images/panda_sitting.png");
+	this.images[1] = gamejs.image.load("images/panda_up.png");
+	this.images[2] = gamejs.image.load("images/panda_down.png");
+	this.images[3] = gamejs.image.load("images/panda_left.png");
+	this.images[4] = gamejs.image.load("images/panda_right.png");
+};
+gamejs.utils.objects.extend(Panda, Animal);
+
+exports.Panda = Panda;
