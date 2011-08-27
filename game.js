db100fa33288d0e381631b3a29aff096e931647e
Joni Lahtinen <joni.lahtinen@gmail.com>
2011-08-27 20:30:27 +0300

    Use += and -=

--- game.js
+++ game.js
@@ -38,10 +38,10 @@ game.playerFired = function(id) {
     , x = panda.x
     , y = panda.y;
   switch (panda.dir) {
-    case params.Direction.UP: y = panda.y - projectileDimensions[1]; break;
-    case params.Direction.DOWN: y = panda.y + params.pandaHeight; break;
-    case params.Direction.LEFT: x = panda.x - projectileDimensions[0]; break;
-    case params.Direction.RIGHT: x = panda.x + params.pandaWidth; break;
+    case params.Direction.UP: y -= projectileDimensions[1]; break;
+    case params.Direction.DOWN: y += params.pandaHeight; break;
+    case params.Direction.LEFT: x -= projectileDimensions[0]; break;
+    case params.Direction.RIGHT: x += params.pandaWidth; break;
   }
   projectiles[id] = {x: x, y: y, dir: panda.dir};
 };
