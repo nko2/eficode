c209ba7a5a8925f18696d5f4c6f7de711a5b8e91
Joni Lahtinen <joni.lahtinen@gmail.com>
2011-08-27 16:42:37 +0300

    Start the game if user presses enter

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,7 @@ var gamejs = require('gamejs'),
 
 $(document).ready(function() {
 	var socket = io.connect();
-	
+
 	var main = function() {
 		$("#login").hide();
 		$("#main").show();
@@ -11,19 +11,25 @@ $(document).ready(function() {
 		display = gamejs.display.setMode([600, 600]);
 		tick.start(display, socket);
 	};
-	
-	$('#start').click(function() {
-    socket.emit('join', $('#nick').val(), function() {
-      gamejs.preload(
-				["images/panda_side_1.png", "images/panda_side_2.png",
-				 "images/panda_down_1.png", "images/panda_down_2.png",
-				 "images/panda_up_1.png", "images/panda_up_2.png",
-				 "images/panda_sitting.png",
-				 "images/flame_bolt_vert_1.png", "images/flame_bolt_vert_2.png",
-				 "images/grass_tile.png"
-				]);
-			
-			gamejs.ready(main);
+
+    function start() {
+        socket.emit('join', $('#nick').val(), function() {
+            gamejs.preload(
+                ["images/panda_side_1.png", "images/panda_side_2.png",
+                    "images/panda_down_1.png", "images/panda_down_2.png",
+                    "images/panda_up_1.png", "images/panda_up_2.png",
+                    "images/panda_sitting.png",
+                    "images/flame_bolt_vert_1.png", "images/flame_bolt_vert_2.png",
+                    "images/grass_tile.png"
+            ]);
+            gamejs.ready(main);
+        });
+    };
+
+    $('#start').click(start);
+    $('#nick').keyup(function(event){
+        if(event.keyCode == 13) {
+            start();
+        };
     });
-  });
 });
