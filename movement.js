7ad2fa108d9f5f0d69b2a4e0e605bb971885234d
Tero Parviainen <tero.parviainen@eficode.fi>
2011-08-27 22:06:28 +0300

    panda collision detection

--- movement.js
+++ movement.js
@@ -1,23 +1,53 @@
 var params = require('./params')
+  , geometry = require('./geometry')
   , _ = require('underscore');
 
 movement = {
-  updatePandaPosition: function(panda) {
-    if (!panda.moving) return;
   
+  findCollidingPandas: function(panda, allPandas) {
+    return _(allPandas).select(function(otherPanda) {
+      if (panda === otherPanda) {
+        return false;
+      } else {
+        return geometry.isRectangleIntersection(panda.x, panda.y, params.pandaWidth, params.pandaHeight,
+                                                otherPanda.x, otherPanda.y, params.pandaWidth, params.pandaHeight);
+      }
+    });
+  },
+  
+  forEachColliding: function(panda, allPandas, fn) {
+    _(this.findCollidingPandas(panda, allPandas)).each(fn);
+  },
+  
+  updatePandaPosition: function(panda, allPandas) {
     var speed = params.Speed.PANDA;
+
+    if (!panda.moving) return;
+
     switch (panda.dir) {
       case params.Direction.UP:
         panda.y = Math.max(panda.y - speed, 0);
+        this.forEachColliding(panda, allPandas, function(collidingPanda) {
+          panda.y = Math.max(panda.y, collidingPanda.y + params.pandaHeight);
+        });
         break;
       case params.Direction.DOWN:
         panda.y = Math.min(panda.y + speed, params.gameHeight - params.pandaHeight);
+        this.forEachColliding(panda, allPandas, function(collidingPanda) {
+          panda.y = Math.min(panda.y + params.pandaHeight, collidingPanda.y) - params.pandaHeight;
+        });
         break;
       case params.Direction.LEFT:
         panda.x = Math.max(panda.x - speed, 0);
+        this.forEachColliding(panda, allPandas, function(collidingPanda) {
+          panda.x = Math.max(panda.x, collidingPanda.x + params.pandaWidth);
+        });
         break;
       case params.Direction.RIGHT:
         panda.x = Math.min(panda.x + speed, params.gameWidth - params.pandaWidth);
+        this.forEachColliding(panda, allPandas, function(collidingPanda) {
+          panda.x = Math.min(panda.x + params.pandaWidth, collidingPanda.x) - params.pandaWidth;
+        });
         break;
     }
   },
@@ -33,7 +63,10 @@ movement = {
   },
 
   updatePositions: function(pandas, projectiles) {
-    _(pandas).each(this.updatePandaPosition);
+    var self = this;
+    _(pandas).each(function(panda) {
+      self.updatePandaPosition(panda, pandas);
+    });
     _(projectiles).each(this.updateProjectilePosition);
   }
 }
