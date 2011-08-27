4624685fca6a76d4fa319c0ea3d640ad7a75839c
Tero Parviainen <tero.parviainen@eficode.fi>
2011-08-27 12:24:14 +0300

    move stuff around

--- game.js
+++ game.js
@@ -1,11 +1,26 @@
 var evt = require('events')
+  , _ = require('underscore')
   , Direction = {NONE: 0, UP: 1, DOWN: 2, LEFT: 3, RIGHT: 4}
+  , frameRate = 2
+  , tankSpeed = 10
   , game = new evt.EventEmitter()
   , state = {};
 
+function updateElementPosition(el) {
+  switch (el.dir) {
+    case Direction.UP:    el['y'] -= tankSpeed; break;
+    case Direction.DOWN:  el['y'] += tankSpeed; break;
+    case Direction.LEFT:  el['x'] -= tankSpeed; break;
+    case Direction.RIGHT: el['x'] += tankSpeed; break;
+  }
+}
+
+function updatePositions() {
+  _(state).each(updateElementPosition);
+};
 
 game.playerJoined = function(nick) {
-  state[nick] = {dir: Direction.NONE};
+  state[nick] = {type: 'panda', x: 0, y: 0, dir: Direction.NONE};
 };
 game.playerLeft = function(nick) {
   delete state[nick];
@@ -21,8 +36,9 @@ game.playerFired = function(nick) {
 };
 
 (function gameLoop() {
+  updatePositions();
   game.emit('state', state);
-  setTimeout(gameLoop, 1000);
+  setTimeout(gameLoop, 1000 / frameRate);
 })();
 
 
