615d661b7c4e365ec1bc89c2bd48d7e435223818
Joni Lahtinen <joni.lahtinen@gmail.com>
2011-08-28 12:10:17 +0300

    Coordinates for palms and sands

---
+++ public/javascripts/sand_coordinates.js
@@ -0,0 +1,14 @@
+sand_coordinates = [ [ 267, 306 ],
+  [ 42, 56 ],
+  [ 598, 85 ],
+  [ 565, 154 ],
+  [ 466, 14 ],
+  [ 455, 269 ],
+  [ 150, 226 ],
+  [ 287, 161 ],
+  [ 732, 488 ],
+  [ 375, 397 ] ];
+// Epic hack
+if (typeof window === 'undefined') {
+  module.exports = sand_coordinates;
+}
