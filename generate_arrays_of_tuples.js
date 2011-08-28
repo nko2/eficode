9cd9cab7fdde6c355668c1dd650b1e8d0cfcb7ac
Joni Lahtinen <joni.lahtinen@gmail.com>
2011-08-28 11:59:36 +0300

    Script to generate array containing points in gamearea

---
+++ generate_arrays_of_tuples.js
@@ -0,0 +1,13 @@
+var count = process.argv[2];
+var name = process.argv[3];
+var tuples = [];
+var _ = require('./public/javascripts/underscore-min.js')._;
+var params = require('./public/javascripts/params.js');
+var util = require('util');
+_(_.range(count)).each(function() {
+    var x, y;
+    x = Math.random() * params.gameWidth;
+    y = Math.random() * params.gameHeight;
+    tuples.push([Math.floor(x),Math.floor(y)]);
+});
+console.log(name + " = " + util.inspect(tuples));
