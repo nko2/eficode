747df45c70a6db9a6cffd8243511adb5ef62f27a
Tero Parviainen <tero.parviainen@eficode.fi>
2011-08-28 20:22:28 +0300

    ipad css fixes

--- public/javascripts/ipad.js
+++ public/javascripts/ipad.js
@@ -100,8 +100,8 @@ var drawMoveControls = function(ctx, canvas, dir) {
   
   ctx.strokeStyle = '#000';
 
-  var upAlpha = 0.6;
-  if (dir === params.Direction.UP) upAlpha = 0.9;
+  var upAlpha = 0.4;
+  if (dir === params.Direction.UP) upAlpha = 0.7;
   ctx.fillStyle = makeLinearGradient(ctx, 100, 100, 100, 0, upAlpha);
   ctx.beginPath();
   ctx.moveTo(0, 0);
@@ -110,8 +110,8 @@ var drawMoveControls = function(ctx, canvas, dir) {
   ctx.closePath();
   ctx.fill();
   
-  var downAlpha = 0.6;
-  if (dir === params.Direction.DOWN) downAlpha = 0.9;
+  var downAlpha = 0.4;
+  if (dir === params.Direction.DOWN) downAlpha = 0.7;
   ctx.fillStyle = makeLinearGradient(ctx, 100, 100, 100, 200, downAlpha);
   ctx.beginPath();
   ctx.moveTo(0, 200);
@@ -120,8 +120,8 @@ var drawMoveControls = function(ctx, canvas, dir) {
   ctx.closePath();
   ctx.fill();
   
-  var leftAlpha = 0.6;
-  if (dir === params.Direction.LEFT) leftAlpha = 0.9;
+  var leftAlpha = 0.4;
+  if (dir === params.Direction.LEFT) leftAlpha = 0.7;
   ctx.fillStyle = makeLinearGradient(ctx, 100, 100, 0, 100, leftAlpha);
   ctx.beginPath();
   ctx.moveTo(0, 0);
@@ -130,8 +130,8 @@ var drawMoveControls = function(ctx, canvas, dir) {
   ctx.closePath();
   ctx.fill();
 
-  var rightAlpha = 0.6;
-  if (dir === params.Direction.RIGHT) rightAlpha = 0.9;
+  var rightAlpha = 0.4;
+  if (dir === params.Direction.RIGHT) rightAlpha = 0.7;
   ctx.fillStyle = makeLinearGradient(ctx, 100, 100, 200, 100, rightAlpha);
   ctx.beginPath();
   ctx.moveTo(200, 0);
@@ -173,8 +173,8 @@ var drawMoveControls = function(ctx, canvas, dir) {
 
 var makeLinearGradient = function(ctx, x1, y1, x2, y2, alpha) {
   var grad = ctx.createLinearGradient(x1, y1, x2, y2);
-  grad.addColorStop(0, 'rgba(150, 150, 150, '+alpha+')');
-  grad.addColorStop(1, 'rgba(120, 120, 120, '+alpha+')');
+  grad.addColorStop(0, 'rgba(50, 50, 50, '+alpha+')');
+  grad.addColorStop(1, 'rgba(20, 20, 20, '+alpha+')');
   return grad;
 }
 
@@ -195,15 +195,15 @@ var addFireControls = function(game) {
    fireCanvas.height= 200;
    var fCtx = fireCanvas.getContext('2d');
 
-   drawFireControls(fCtx, fireCanvas, 0.6);
+   drawFireControls(fCtx, fireCanvas, 0.4);
    
    $(document.body).append(fireControls);
        
    fireCanvas.addEventListener('touchstart', function(evt) {
      game.fire();
-     drawFireControls(fCtx, fireCanvas, 0.9);
+     drawFireControls(fCtx, fireCanvas, 0.7);
      setTimeout(function() {
-       drawFireControls(fCtx, fireCanvas, 0.6);
+       drawFireControls(fCtx, fireCanvas, 0.4);
      }, 100);
      evt.preventDefault();
      return false;
@@ -216,8 +216,8 @@ var drawFireControls = function(fCtx, canvas, alpha) {
   fCtx.strokeStyle = '#000';
   fCtx.fillStyle = '#ccc';
   var fireGrad = fCtx.createRadialGradient(100, 100, 50, 100, 100, 150);      
-  fireGrad.addColorStop(0, 'rgba(150, 150, 150, '+alpha+')');
-  fireGrad.addColorStop(1, 'rgba(120, 120, 120, '+alpha+')');
+  fireGrad.addColorStop(0, 'rgba(50, 50, 50, '+alpha+')');
+  fireGrad.addColorStop(1, 'rgba(20, 20, 20, '+alpha+')');
   fCtx.fillStyle = fireGrad;
   
   fCtx.fillRect(0, 0, 200, 200);
