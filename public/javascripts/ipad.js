20af00c258f7201a99582f6f173225b34614f95e
Tero Parviainen <tero.parviainen@eficode.fi>
2011-08-28 18:08:03 +0300

    ipad updates

--- public/javascripts/ipad.js
+++ public/javascripts/ipad.js
@@ -5,8 +5,8 @@ var start = function(game, socket) {
   //if (isPad) {
     $('<meta name="viewport" content="width=device-width,maximum-scale=1.0" />').appendTo($('head'));
     $('.container_10').css({
-      width: '1024px',
-      height: '690px',
+      width: '960px',
+      height: '680px',
       minWidth: '1024px'
     });
     $('#header, #instructions').hide();
