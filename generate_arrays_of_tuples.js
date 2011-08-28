f730a7d7efcaf50fc952001c996ef2b70391d62f
Joni Lahtinen <joni.lahtinen@gmail.com>
2011-08-28 15:45:31 +0300

    Allow to disable rotation

--- generate_arrays_of_tuples.js
+++ generate_arrays_of_tuples.js
@@ -1,15 +1,20 @@
 var count = process.argv[2];
 var name = process.argv[3];
 var export = process.argv[4];
+var noRotate = process.argv[5];
 var tuples = [];
 var _ = require('./public/javascripts/underscore-min.js')._;
 var params = require('./public/javascripts/params.js');
 var util = require('util');
 _(_.range(count)).each(function() {
-    var x, y;
+    var x, y, r;
     x = Math.random() * params.gameWidth;
     y = Math.random() * params.gameHeight;
-    r = Math.random() * 360;
+    if( undefined === noRotate) {
+        r = Math.random() * 360;
+    } else {
+        r = 0;
+    }
     tuples.push([Math.floor(x),Math.floor(y),Math.floor(r)]);
 });
 console.log("var " + name + " = " + util.inspect(tuples) + ";");
